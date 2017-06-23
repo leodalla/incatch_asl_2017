5759c049ce0a781c25047981a51adc69dcc824bc
Leonardo <leo00.dallariva@gmail.com>
2017-06-23 15:41:08 +0200

    15 41

--- web/asl/code/Framework.java
+++ web/asl/code/Framework.java
@@ -1,4 +1,43 @@
-package incatch;
+/*
+ *  Intcatch ASL giugno 2017 Background Subtraction Library multi-thread
+ *  Copyright 2017 Domenico Bloisi, Leonardo Dalla Riva, Carlo Bottaro.
+ *
+ *  This file is part of Intcatch ASL giugno 2017 and it is distributed under the terms of the
+ *  GNU Lesser General Public License (Lesser GPL)
+ *
+ *  
+ *
+ *  Intcatch ASL giugno 2017 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Intcatch ASL giugno 2017 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Intcatch ASL giugno 2017.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  This file contains the C++ OpenCV based implementation for
+ *  Intcatch ASL giugno 2017 algorithm described in
+ *  
+ *  Domenico D. Bloisi, Carlo Bottaro and Leonardo Dalla Riva
+ *  "Intcatch ASL giugno 2017"
+ *  Pattern Recognition Letters
+ *
+ *  Please, cite the above paper if you use Intcatch ASL giugno 2017.
+ *
+ *  Intcatch ASL giugno 2017 has been written by Domenico D. Bloisi, Carlo Bottaro and Leonardo Dalla Riva
+ *
+ *  Please, report suggestions/comments/bugs to
+ *  leo00.dallariva@gmail.com
+ *  bottarocarloo@gmail.com
+ *
+ */
+
+package intcatch;
 
 import java.util.Vector;
 import java.util.*;
@@ -33,14 +72,15 @@ public class Framework extends JPanel implements ActionListener{
                     logButton,
                     graphButton,
                     riconoscimentiButton;
-    private JPanel buttonPanel,progressPanel;
+    private JPanel buttonPanel;
+    private String currentLogFile;
     private String mapname;
     //actions
     //0 standby
     //1 play
     //2 stop
     //3 next
-    private int actions;
+    private int actions, initialPoint,finishPoint;
     private Vector<Pose> log;
     private Map map;
     
@@ -168,18 +208,54 @@ public class Framework extends JPanel implements ActionListener{
             mapButton.setEnabled(false);
             
         }
-                Graphics2D g2d = mapImage.createGraphics();
-                g2d.setColor(Color.RED); 
-                Iterator<Pose> it= log.iterator();
+               
                 
+                Iterator<Pose> it= log.iterator();
+               
+                Graphics2D g2d = mapImage.createGraphics();
+                Point2D currentPoint=null;
+                Point2D previusPoint=null;
+                double previusAng=0;
+                double angolo=0;
+                int colore=0;
                 while(it.hasNext()){
+                    
                     while((actions==1)&&(it.hasNext())){
+                       
                         Pose p = it.next();
                         Point2D puntoImmagine = map.convert(p);
-                        g2d.fillOval((int)(puntoImmagine.getX()), (int)(puntoImmagine.getY()), 3, 3);
+                        previusPoint=currentPoint;
+                        currentPoint=puntoImmagine;
+                        previusAng=angolo;
+                        angolo=angleColor(previusPoint,currentPoint);
+                        colore=angle(angolo,previusAng,colore);
+                        if(colore==1){
+                            g2d.setColor(Color.red);
+                        }
+                        if(colore==2){
+                            g2d.setColor(Color.black);       
+                        }
+                        if(colore==3){
+                            g2d.setColor(Color.blue);
+                        }
+                        
+                        int punto_x = (int)(puntoImmagine.getX());
+                        int punto_y = (int)(puntoImmagine.getY());
+                        
+                        /*
+                        cambiare
+                        
+                        punto_x += 13700;
+                        punto_x *= 3;
+                        punto_y += 41200;
+                        punto_y *= 3;
+                        */
+                        
+                        g2d.fillOval(punto_x, punto_y, 4, 4);
+                        System.out.println("punto immagine  "  + punto_x + " " + punto_y);
                         frame.revalidate();
                         frame.repaint();
-                   }
+                    }
                     while(actions==2){
                             stopButton.setEnabled(false);
                             try{
@@ -187,35 +263,101 @@ public class Framework extends JPanel implements ActionListener{
                             }
                             catch(InterruptedException e){}
                     }
+                    //NEXTTTTTT
                     if(actions==3){
+                        actions=3;
+                        while((actions==3)&&(it.hasNext())){
+                           Pose p = it.next();
+                        Point2D puntoImmagine = map.convert(p);
+                        previusPoint=currentPoint;
+                        currentPoint=puntoImmagine;
+                        previusAng=angolo;
+                        angolo=angleColor(previusPoint,currentPoint);
+                        colore=angle(angolo,previusAng,colore);
+                        if(colore==1){
+                            g2d.setColor(Color.red);
+                        }
+                        if(colore==2){
+                            g2d.setColor(Color.black);       
+                        }
+                        if(colore==3){
+                            g2d.setColor(Color.blue);
+                        }
+                        g2d.fillOval((int)(puntoImmagine.getX()), (int)(puntoImmagine.getY()), 4, 4);
+                        frame.revalidate();
+                        frame.repaint();
                         actions=0;
-                        while((actions==0)&&(it.hasNext())){
-                            Pose p = it.next();
-                            Point2D puntoImmagine = map.convert(p);
-                            g2d.fillOval((int)(puntoImmagine.getX()), (int)(puntoImmagine.getY()), 3, 3);
-                            frame.revalidate();
-                            frame.repaint();
-                            actions=2;
+                        while(actions==0){
+                            try{
+                                Thread.sleep(30);
+                            }
+                            catch(InterruptedException e){}
+                        }
                         }  
                     }
                     
                 }
-                int reply = JOptionPane.showConfirmDialog(null, "REFRESH?", "BHO INTANTO VEDIAMO SE DA FUNZIA", JOptionPane.YES_NO_OPTION);
+                int reply = JOptionPane.showConfirmDialog(null, "RICARICARE?", "---", JOptionPane.YES_NO_OPTION);
                 if (reply == JOptionPane.YES_OPTION) {
-                //JOptionPane.showMessageDialog(null, "DAMMI UN SECONDO CHE PULISCO LA MAPPA");
-                MapReader mr = new MapReader(mapname);
-                map = mr.getMap();
-                addMap();                  
-                // mapChooser();
-                  actions=2;
+                    setVisible(false); //you can't see me!
+                    frame.dispose(); //Destroy the JFrame object
+                    return -2;
+                    
                 }
                 else {
-                   JOptionPane.showMessageDialog(null, "OTTIMA SCELTA");
-                   
+                   //JOptionPane.showMessageDialog(null, "OTTIMA SCELTA");
                 }
+        
                 
-            actions=2;
-        return 0;
+                return 0;
+        }
+    
+    public double angleColor(Point2D b,Point2D a){
+        //1 dritto rosso
+        //2 destra verde
+        //3 sinistra blu
+        if(a==null||b==null){
+            return 1;
+        }
+        double ax=a.getX();
+        double ay=a.getY();
+        double bx=b.getX();
+        double by=b.getY();
+        double ang=Math.tan(ax-bx/ay-by);
+        if(ang<0){
+            ang+=360;
+        }
+        System.out.println("ang: " + ang);
+        return ang;
+        
+        
+    }
+    public int angle(double ang, double pAng, int cl){
+        
+        
+        if(ang==pAng){
+            
+            return cl;
+        }
+        else {
+            if((ang<2||ang>358)||(ang>178&&ang<182)){
+               // System.out.println("1");
+                return 1;
+            }
+            else{
+                if(ang>180){
+                 //   System.out.println("2");
+                    return 2;
+                }
+                else{
+                   // System.out.println("3");
+                    return 3;
+                }
+            }
+        }
+          
+            
+          
     }
     
     public void actionPerformed(ActionEvent e) {
@@ -252,11 +394,16 @@ public class Framework extends JPanel implements ActionListener{
             logButton.setEnabled(false);
         }
         else if(command.equals("graph")){
-            createGraph();
+            Graph graph=  createGraph();
+           
+            initialPoint=valore("initial point");
+            finishPoint=valore("final point");
+            runDijkstra(graph);
         }
         else if(command.equals("rico")){
             rico();
         }
+         
     }
  
     public void mapChooser() {
@@ -287,40 +434,42 @@ public class Framework extends JPanel implements ActionListener{
             int n = fileChooser.showOpenDialog(Framework.this);
             if (n == JFileChooser.APPROVE_OPTION) {
                 File f = fileChooser.getSelectedFile();
-                String nomefile= new String(f.toString());
-                LogReader lr= new LogReader(nomefile);
+                currentLogFile = new String(f.toString());
+                LogReader lr = new LogReader(currentLogFile);
                 
                 if(lr.read()){
-                   
+                   System.out.print("Reading log...");
+                   System.out.flush();
                 }
-                else{
-                   System.out.println("Errore chiudo"); 
+                else {
+                   System.out.println("Error in reading log"); 
                    return;
-
                 }
+                
+                System.out.println("[OK]");
+                
                 log = lr.getVector();
-               
+
+                log = riduciLog(log, 30);
+                
             }
-             } 
+        } 
         catch (Exception ex) {
-             }
+        }
     }
     
-    public void createGraph() {
+    public Graph createGraph() {
         int cont=0;
-        
+       
         //progressPanel =new JPanel(new FlowLayout(FlowLayout.LEFT));
         
         graphFrame = new JFrame("Grafo");
                 
-        graphFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
-                
-                
-        graphPanel= new GraphDraw();
+        graphFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+        graphPanel= new GraphDraw(2);
        
-        graphFrame.setSize((int)(mapImage.getWidth()),
-                               (int)(mapImage.getHeight()));
+        graphFrame.setSize((int)(mapImage.getWidth()*1.3),
+                               (int)(mapImage.getHeight()*1.3));
        
         JProgressBar progressBar = new JProgressBar(0, 100);
         JFrame progressFrame= new JFrame();
@@ -328,21 +477,17 @@ public class Framework extends JPanel implements ActionListener{
         progressBar.setStringPainted(true);
         JPanel panel = new JPanel();
         panel.add(progressBar);
-        graphFrame.getContentPane().add(playButton,BorderLayout.NORTH);
         graphFrame.getContentPane().add(panel,BorderLayout.NORTH);
         graphFrame.getContentPane().add(graphPanel,BorderLayout.CENTER);
- 
         //panel.add(progressBar);
         progressFrame.getContentPane().add(panel,BorderLayout.NORTH);
         progressFrame.setSize(300, 300);
-        progressFrame.setVisible(true);
+        progressFrame.setVisible(false);
         progressFrame.repaint();
         graphFrame.setVisible(true);
-        
         double temp_x=0,temp_y=0;
-        
         double max=-1;
-        //log is a vector of <Pose>
+
         Iterator<Pose> it = log.iterator();
         while(it.hasNext()) {
             Pose pose = it.next();
@@ -352,21 +497,16 @@ public class Framework extends JPanel implements ActionListener{
                         cont++;
                         //System.out.println("cont: "+ cont);
                         progressBar.setValue(cont);
-                        progressBar.setVisible(true);
-                        
-                       
+                        progressBar.setVisible(false);
             ArrayList<Node> nodes = graphPanel.getNodes();
-            
             Node n = new Node(nodes.size(),
-                        point.getX(),
-                        point.getY(),
-                        (int)x,
-                        (int)y);
-            
-            temp_x=point.getX();
-            temp_y=point.getY();
-            Pose oldpose=null;
-            
+                point.getX(),
+                point.getY(),
+                (int)x,
+                (int)y);
+                temp_x=point.getX();
+                temp_y=point.getY();
+                Pose oldpose=null;
             for(int i=0;i<log.size();i++)
             {
                 
@@ -381,29 +521,20 @@ public class Framework extends JPanel implements ActionListener{
                     Point2D p2 = map.convert(oldpose);
                     double d = Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) +
                          Math.pow(p1.getY() - p2.getY(), 2));
-                    
                     //System.out.println("d: "+ d);
                     if(max<d)
                     {
                         max=d;
-                        System.out.println("max: "+ max);
+                        //System.out.println("max: "+ max);
                     }
                     oldpose=p;
-                }
-                
-                
+                }    
             }
-            
-            
             for (Node node : nodes) {
-                
-                
                // System.out.println(node.toString());
                 double d = GraphDraw.distanceBetweenUTM( x, y, node.getX(), node.getY());
-                       
-                
                 //System.out.println("d: "+ d);
-                if(d < max && d > 0) {
+                if(d <= max && d >= 0) {
                     graphPanel.addEdge(node.getIdx(), n.getIdx());
                 }
             }
@@ -421,7 +552,106 @@ public class Framework extends JPanel implements ActionListener{
             graphFrame.repaint();
         }
         
-        System.out.println("finito" +max);
+        Graph graph = generateGraph(log, max);
+        graph.print();
+        return graph;
+        
+    }
+    
+    private void runDijkstra(Graph graph){
+        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
+        ArrayList<Node> nodes = graph.getNodes();
+        dijkstra.execute(nodes.get(initialPoint));
+        LinkedList<Node> path = dijkstra.getPath(nodes.get(finishPoint));
+        System.out.println("getpathortest Path:");
+        for (Node n : path) {
+            System.out.println(n.toString());
+        }        
+    }
+    
+    private Vector<Pose> riduciLog(Vector<Pose> log, int new_size){
+        Vector<Pose> ridotto=new Vector<Pose>();
+        Iterator<Pose> it = log.iterator();
+         
+        int skip_value= log.size()/new_size;
+        while(it.hasNext()){
+            ridotto.add(it.next());
+            for(int i=0;i<skip_value;i++){
+                if(it.hasNext()){
+                    it.next();
+                }
+                else{
+                    break;
+                }
+            }
+        }        
+        return ridotto;
+    }
+    
+    private Graph generateGraph(Vector<Pose> poses, double max_dist)
+    {
+        ArrayList<Node> nodes = new ArrayList<Node>();        
+        Iterator<Pose> it = poses.iterator();
+        int i = 0;
+        while(it.hasNext()){            
+            Pose pose = it.next();
+            Point2D point = map.convert(pose);
+            double x = point.getX();
+            double y = point.getY();
+            Node location = new Node(String.valueOf(i),
+                                    String.valueOf(i),
+                                    nodes.size(),
+                                    point.getX(),
+                                    point.getY(),
+                                    (int)x,
+                                    (int)y);
+            
+            
+            System.out.println("Nodo " + i + " UTM " + pose.getP());
+            
+            nodes.add(location);
+            i++;
+        }
+        
+        ArrayList<Edge> edges = new ArrayList<Edge>();
+        
+        int j = 0;
+        for (Node n : nodes) {
+            for (Node m : nodes) {
+                double d = Graph.distanceBetweenUTM(n.getX(),
+                                                    n.getY(),
+                                                    m.getX(),
+                                                    m.getY());
+                if(d <= max_dist && d > 0) {
+                                       
+                    Edge e = new Edge(  String.valueOf(j),
+                                        nodes.get(n.getIdx()),
+                                        nodes.get(m.getIdx()),
+                                        d);
+                    edges.add(e);
+                    j++;
+                }
+            }
+        }
+               
+        return new Graph(nodes, edges);
+        
+    }
+    
+    public int valore(String input){
+        Object[] possibilities = {"1","2","3","4","5","6","7","8","9", "10","11","12","13","14","15","16","17","18","19", "20","26"};
+        String s = (String)JOptionPane.showInputDialog(
+                    frame,
+                    "Choose a number: \n it's for DijkstraAlgorithm.."
+                    + input,
+                    "   ",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    possibilities,
+                    "10");
+        int b = Integer.parseInt(s);
+
+        return b;
     }
 }
 
